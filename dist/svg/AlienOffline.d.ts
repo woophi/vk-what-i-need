b64ea1b4b3d5502231eb65fc4b365a7620deb191
konst <konstantin.mikheev@icord.cz>
2020-12-17 15:29:41 +0100

    alien svg

---
+++ dist/svg/AlienOffline.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+declare type Props = {
+    className?: string;
+    style?: React.CSSProperties;
+};
+export declare const AlienOffline: React.NamedExoticComponent<Props>;
+export {};
